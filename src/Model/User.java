bb50195454602303c5a9ca5a73c928399dd05866
Phạm Trọng Nhân <tendaigom@gmail.com>
2019-10-22 16:17:07 +0700

    Project structure

--- src/Model/User.java
+++ src/Model/User.java
@@ -1,10 +1,17 @@
 package Model;
 
-class User {
+public class User {
     private int id;
     private String user_name;
     private String password;
     private String nick_name;
+
+    User(int id, String user_name, String password, String nick_name) {
+        this.id = id;
+        this.user_name = user_name;
+        this.password = password;
+        this.nick_name = nick_name;
+    }
 
     public String getNick_name() {
         return nick_name;
