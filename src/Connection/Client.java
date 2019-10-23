d05a05a1ae1685c2cd0c43e2f3e22a6fc721dfb9
Huynh Ngoc Thien <gunshot1st@gmail.com>
2019-10-23 12:24:12 +0700

    OK

--- src/Connection/Client.java
+++ src/Connection/Client.java
@@ -1,4 +1,22 @@
 package Connection;
 
+import java.net.*;
+import java.io.*;
+
 public class Client {
+
+    private Socket socket = null;
+
+    public Client(String address, int port) throws IOException {
+        try
+        {
+            socket = new Socket(address, port);
+            System.out.println("Connected");
+        }
+        catch(UnknownHostException u)
+        {
+            System.out.println(u);
+        }
+    }
+
 }
