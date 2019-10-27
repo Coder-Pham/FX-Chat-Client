0ef5bf14ca28af770123d0421ac93f302ca4ea3f
Huynh Ngoc Thien <gunshot1st@gmail.com>
2019-10-28 02:41:44 +0700

    Finish Message UI, create global.css and add stylesheet inside fxml

--- src/Controller/Message.java
+++ src/Controller/Message.java
@@ -1,29 +1,17 @@
 package Controller;
 
-import Connection.ServerHandler;
-import Model.StageView;
 import Model.Friend;
-import Helper.MessageHelper;
 
 import com.jfoenix.controls.JFXButton;
-import javafx.application.Application;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -39,8 +27,6 @@ public class Message implements Initializable {
 
     @FXML
     private Label userNickName;
-
-    private MessageHelper mh;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
